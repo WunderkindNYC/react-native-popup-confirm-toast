4c97c9b4ea5ed103c476bf5480033018c51391cc
Jose Gomez <jose4gga@gmail.com>
2022-05-10 12:21:54 -0400

    clean Root.js

--- src/main/Root.js
+++ src/main/Root.js
@@ -1,46 +1,40 @@
-import React, {Component} from 'react';
-import {Dimensions, View, ViewPropTypes} from 'react-native';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Popup from './Popup';
 import Toast from './Toast';
 import SPSheet from './SPSheet';
 
-const {width, height} = Dimensions.get('window');
+function Root() {
+    return (
+        <>
+            {this.props.children}
+            <Popup
+                ref={c => {
+                    if (c) {
+                        Popup.popupInstance = c;
+                    }
+                }}
+            />
 
-class Root extends Component {
-    render() {
-        return (
-            <>
-                {this.props.children}
-                <Popup
-                    ref={c => {
-                        if (c) {
-                            Popup.popupInstance = c;
-                        }
-                    }}
-                />
-
-                <Toast
-                    ref={c => {
-                        if (c) {
-                            Toast.toastInstance = c;
-                        }
-                    }}
-                />
-                <SPSheet
-                    ref={c => {
-                        if (c) {
-                            SPSheet.spsheetInstance = c;
-                        }
-                    }}
-                />
-            </>
-        );
-    }
+            <Toast
+                ref={c => {
+                    if (c) {
+                        Toast.toastInstance = c;
+                    }
+                }}
+            />
+            <SPSheet
+                ref={c => {
+                    if (c) {
+                        SPSheet.spsheetInstance = c;
+                    }
+                }}
+            />
+        </>
+    );
 }
 
 Root.propTypes = {
-    ...ViewPropTypes,
     style: PropTypes.oneOfType([
         PropTypes.object,
         PropTypes.number,
